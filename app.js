0687c4c57dff08ad99d7a341c9e09a2b30146c2d
Attila Bongar <bongar007@gmail.com>
2021-01-27 02:01:09 -0500

    more fixes

--- app.js
+++ app.js
@@ -64,4 +64,6 @@ app.use("/users", require("./routes/users.js"));
 
 const port = 3000;
 
-app.listen(port, console.log(`Server running on  ${port}`));
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server listening on port 3000");
+});
